9a9e1461f539604fbd40ee89fceb0bd33b1b129c
maxmay94 <maxmay94@gmail.com>
2023-02-24 13:17:49 -0500

    add more tweaks to debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import Background from './components/Background'
-import TextZone from './components/TextZone'
 import { PresentationControls } from '@react-three/drei'
 import { useControls } from 'leva'
+
+import Background from './components/Background'
+import TextZone from './components/TextZone'
+import Shape from './components/Shape'
 
 function App() {
 
@@ -14,7 +16,20 @@ function App() {
       mass: 1,
       tension: 500
     },
+    shapeCount:{
+      value: 3,
+      min: 0,
+      max:10,
+      step: 1
+    }
   })
+  
+  let shapeCount = cameraControls.shapeCount
+  const shapes = []
+  for(let i = 0; i < shapeCount; i++) {
+    shapes.push(<Shape key={i} />)
+  }
+
 
   return (
     <>
@@ -27,7 +42,8 @@ function App() {
         polar={[-Math.PI / 3, Math.PI / 3]}
         azimuth={[-Math.PI / 1.4, Math.PI / 2]}
       >
-        <TextZone />
+      <TextZone />
+      {shapes}
       </PresentationControls>
     </>
   )
--- src/components/Shape.jsx
+++ src/components/Shape.jsx
@@ -1,8 +1,70 @@
+import * as THREE from 'three'
+import { LayerMaterial, Depth, Noise, Fresnel } from 'lamina'
+import { Float } from '@react-three/drei'
+import { useState } from 'react'
+import { useControls } from 'leva'
+
+
 function Shape() {
+
+  const shapeControls = useControls('shapes', {
+    baseColor: '#00330c',
+    colorA: '#23600d',
+    colorB: '#44634c',
+    noiseScale: {
+      value: 1000,
+      min: 0,
+      max: 1000,
+      step: 1
+    }
+  })
+
+  const [speed] = useState(() => 0.1 + Math.random() / 5)
+  const [position] = useState(() => [(Math.random() * 6) - 3, (Math.random() * 6) - 3, (Math.random() * 6) - 3])
+  const [scale] = useState(() => Math.random())
 
   return(
     <>
-      
+      <Float speed={speed} position={position} rotationIntensity={10} floatIntensity={40}>
+        <mesh scale={scale}>
+          <LayerMaterial color={shapeControls.colorA}>
+            <Depth 
+              colorA={shapeControls.colorA}
+              colorB={shapeControls.colorB}
+              alpha={0.5}
+              mode={'normal'}
+              near={0}
+              far={2}
+              origin={[1,1,1]}
+            />
+            <Depth 
+              colorA={shapeControls.baseColor}
+              colorB={shapeControls.colorB}
+              alpha={0.5}
+              mode={'add'}
+              near={3}
+              far={2}
+              origin={[1,1,1]}
+            />
+              <Fresnel 
+                mode={'add'} 
+                color={new THREE.Color('#E7B473').convertSRGBToLinear()} 
+                intensity={0.3}
+                power={2.5}
+                bias={0.0}
+              />
+            <Noise 
+              mapping={'local'}
+              type={'simplex'}
+              scale={shapeControls.noiseScale}
+              colorA={'#ffaf40'}
+              colorB={'black'}
+              mode={'overlay'}
+            />
+          </LayerMaterial>
+          <torusGeometry />
+        </mesh>
+      </Float>
     </>
   )
 }
